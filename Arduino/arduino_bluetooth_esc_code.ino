11422d1e0b010de6dcc750d843559ea69d753bf0
hanyuanye <32118242+hanyuanye@users.noreply.github.com>
2018-04-27 13:47:41 -0400

    Arduino Code.
    The ESC file deals with the case where the circuit has a Speed Control, e.g. a flying drone.
    The PWM file deals with the case where the Arduino is communicated directly with the motor, e.g. an RC car.

---
+++ Arduino/arduino_bluetooth_esc_code.ino
@@ -0,0 +1,57 @@
+#include <Time.h>
+#include <Servo.h>
+#define MIN_PULSE_LENGTH 1000
+#define MAX_PULSE_LENGTH 2000
+#define PANIC_TIMER 1000
+#define PANIC_PULSE 1500
+#define PIN_ONE 4
+#define PIN_TWO 5
+#define PIN_THREE 6
+#define EMPTY_INPUT 0
+#define NO_INPUT -1
+Servo MOTOR_ONE, MOTOR_TWO, MOTOR_THREE;
+int inputDirection = EMPTY_INPUT;
+int inputPulse = MIN_PULSE_LENGTH;
+time_t startingTime;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  MOTOR_ONE.attach(PIN_ONE, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+  MOTOR_TWO.attach(PIN_TWO, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+  MOTOR_THREE.attach(PIN_THREE, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+  startingTime = time(NULL);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (Serial.available() > 0) {
+    if (inputDirection == EMPTY_INPUT || inputDirection == NO_INPUT) {
+      inputDirection = Serial.read();
+      continue;
+    }
+    inputPulse = inputPulse * 10 + Serial.read();
+  }
+  if (inputDirection == NO_INPUT) {
+    setEscPulse(inputPulse);
+    startingTime = time(NULL);
+    inputPulse = 0;
+    inputDirection = NO_INPUT;
+  }
+  double elapsedTime = difftime(time(NULL), startingTime);
+  if (elapsedTime > PANIC_TIMER) {
+    panicDoSomething();
+  }
+}
+
+void setEscPulse(int inputPulse) {
+  MOTOR_ONE.writeMicroseconds(inputPulse);
+  MOTOR_TWO.writeMicroseconds(inputPulse);
+  MOTOR_THREE.writeMicroseconds(inputPulse);
+}
+
+void panicDoSomething() {
+  MOTOR_ONE.writeMicroseconds(PANIC_PULSE);
+  MOTOR_TWO.writeMicroseconds(PANIC_PULSE);
+  MOTOR_THREE.writeMicroseconds(PANIC_PULSE);
+}
+
---
+++ Arduino/arduino_bluetooth_pwm_code.ino
@@ -0,0 +1,59 @@
+#include <Time.h>
+
+
+const int UP = 1;
+const int DOWN = 0;
+const int EMPTY_INPUT = -1;
+const int NO_INPUT = -2;
+int inputDirection = EMPTY_INPUT;
+int inputPWM = 0;
+int MOTOR_ONE_PIN = 3;
+int MOTOR_TWO_PIN = 5;
+int MOTOR_THREE_PIN = 6;
+time_t startingTime;
+int PANIC_TIMER = 1000; //needs to be adjusted
+int PANIC_PWM = 100; //needs to be adjusted
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(MOTOR_ONE_PIN, OUTPUT);
+  pinMode(MOTOR_TWO_PIN, OUTPUT);
+  pinMode(MOTOR_THREE_PIN, OUTPUT);
+  startingTime = time(NULL);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (Serial.available() > 0) {
+    if (inputDirection == EMPTY_INPUT || inputDirection == NO_INPUT) {
+      inputDirection = Serial.read();
+      continue;
+    }
+    inputPWM = inputPWM * 10 + Serial.read();
+  }
+  if (inputDirection == NO_INPUT) {
+    setMotorPwm(inputPWM);
+    startingTime = time(NULL);
+    inputPWM = 0;
+    inputDirection = NO_INPUT;
+  }
+  double elapsedTime = difftime(time(NULL), startingTime);
+  if (elapsedTime > PANIC_TIMER) {
+    panicDoSomething();
+  }
+  setMotorPwm(PANIC_PWM);
+}
+
+void setMotorPwm(int input) {
+  analogWrite(MOTOR_ONE_PIN, input);
+  analogWrite(MOTOR_TWO_PIN, input);
+  analogWrite(MOTOR_THREE_PIN, input);
+}
+
+void panicDoSomething() {
+  analogWrite(MOTOR_ONE_PIN, PANIC_PWM);
+  analogWrite(MOTOR_TWO_PIN, PANIC_PWM);
+  analogWrite(MOTOR_THREE_PIN, PANIC_PWM);
+}
+
+
